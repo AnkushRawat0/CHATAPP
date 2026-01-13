c60a31ceea7bfdb95a77a307226c77b8c0adc290
AnkushRawat0 <ankushrawat004@gmail.com>
2026-01-14 01:27:43 +0530

    checked cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,13 +9,13 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 const app = express();
-
-app.use(express.json());
-app.use(cookieParser()) ; 
 app.use(cors({
   origin : "http://localhost:5173" ,
   credentials : true ,
 }))
+
+app.use(express.json());
+app.use(cookieParser()) ; 
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
