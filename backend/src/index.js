ac5dc05b4a0049560e63f64814de57b0e75d901e
AnkushRawat0 <ankushrawat004@gmail.com>
2026-01-18 19:24:34 +0530

    added socket server

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,26 +3,29 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { io, server, app } from "./lib/socket.js";
 
-dotenv.config();
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
-const app = express();
-app.use(cors({
-  origin : "http://localhost:5173" ,
-  credentials : true ,
-}))
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(cookieParser()) ; 
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on Port ${PORT}`);
   connectDB();
 });
