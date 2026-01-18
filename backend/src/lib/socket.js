af4d85398f4d9515ac10fc3f810a18d2e8d1ce56
AnkushRawat0 <ankushrawat004@gmail.com>
2026-01-18 19:24:11 +0530

    connections

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,15 +1,22 @@
-import {Server } from "socket.io" ; 
-import express from "express" ;
-import https from "https" ;
+import { Server } from "socket.io";
+import express from "express";
+import https from "https";
 
+const app = express();
+const server = https.createServer(app);
 
-const app = express() ; 
-const server = https.createServer(app) ;
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173"],
+  },
+});
 
-const io = new Server(server ,{
-    cors:{
-        origin : ["http://localhost:5173" ]
-    }
-})
+io.on("connection", (socket) => {
+  console.log("A user connected with id :", socket.id);
 
-export {io , app, server} ;
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+  });
+});
+
+export { io, app, server };
