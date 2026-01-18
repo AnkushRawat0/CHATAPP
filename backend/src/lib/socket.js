1b45bcedbfb048ff951a24a78bf3a63f1dd9735e
AnkushRawat0 <ankushrawat004@gmail.com>
2026-01-18 20:21:48 +0530

    used http instead of https

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,13 +1,16 @@
 import { Server } from "socket.io";
 import express from "express";
-import https from "https";
+import http from "http";
 
 const app = express();
-const server = https.createServer(app);
+const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: "http://localhost:5173" ,
+    credentials: true,
+        
+  
   },
 });
 
